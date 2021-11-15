0aabd867f39d92ad8db7cc0b46dac1f430b080d3
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-15 08:39:11 +0300

    Add precompiled assets for Heroku

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class Config:
   #PHOTOS UPLOAD CONFIGURATION
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+
 class ProdConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
   if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
