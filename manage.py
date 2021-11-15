f2727ef53ec255d51220cb8da2d0fdbbaf3e429d
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-11-15 19:52:10 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -22,5 +22,7 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
 if __name__ == "__main__":
+    app.debug = True
     manager.run()
